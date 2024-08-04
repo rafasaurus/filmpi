38ac25acb487a8aefb23710768e151ba3a209567
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-08-04 14:13:10 +0400

    Working cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,74 +3,62 @@
 #include <iostream>
 #include <vector>
 
-cv::Mat applyHaldClut(const cv::Mat& haldImg, const cv::Mat& img) {
-    // Convert haldImg from BGR to RGB
-    cv::Mat haldImgRGB;
-    cv::cvtColor(haldImg, haldImgRGB, cv::COLOR_BGR2RGB);
+cv::Vec3b trilinearInterpolate(const cv::Mat& haldImg, float clutR, float clutG, float clutB, int clutSize) {
+    int r0 = std::floor(clutR);
+    int r1 = std::min(r0 + 1, clutSize - 1);
+    int g0 = std::floor(clutG);
+    int g1 = std::min(g0 + 1, clutSize - 1);
+    int b0 = std::floor(clutB);
+    int b1 = std::min(b0 + 1, clutSize - 1);
 
-    int haldW = haldImgRGB.cols;
-    int haldH = haldImgRGB.rows;
-    int channels = haldImgRGB.channels();
-    int clutSize = std::round(std::pow(haldW, 1.0 / 3.0));
-    float scale = (clutSize * clutSize - 1) / 255.0f;
+    float rRatio = clutR - r0;
+    float gRatio = clutG - g0;
+    float bRatio = clutB - b0;
 
-    // Reshape haldImg into a 2D array of RGB values
-    cv::Mat haldImgReshaped = haldImgRGB.reshape(1, clutSize * clutSize * clutSize);
-    haldImgReshaped.convertTo(haldImgReshaped, CV_32F);
+    cv::Vec3b c000 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b0));
+    cv::Vec3b c100 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b0));
+    cv::Vec3b c010 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b0));
+    cv::Vec3b c110 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b0));
+    cv::Vec3b c001 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b1));
+    cv::Vec3b c101 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b1));
+    cv::Vec3b c011 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b1));
+    cv::Vec3b c111 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b1));
 
-    // Convert img to float
-    cv::Mat imgFloat;
-    img.convertTo(imgFloat, CV_32F);
+    cv::Vec3b c00 = c000 * (1 - bRatio) + c100 * bRatio;
+    cv::Vec3b c01 = c001 * (1 - bRatio) + c101 * bRatio;
+    cv::Vec3b c10 = c010 * (1 - bRatio) + c110 * bRatio;
+    cv::Vec3b c11 = c011 * (1 - bRatio) + c111 * bRatio;
 
-    // Figure out the 3D CLUT indexes corresponding to the pixels in our image
-    cv::Mat clutR, clutG, clutB;
-    cv::Mat imgChannels[3];
-    cv::split(imgFloat, imgChannels);
+    cv::Vec3b c0 = c00 * (1 - gRatio) + c10 * gRatio;
+    cv::Vec3b c1 = c01 * (1 - gRatio) + c11 * gRatio;
 
-    clutR = imgChannels[0] * scale;
-    clutG = imgChannels[1] * scale;
-    clutB = imgChannels[2] * scale;
+    cv::Vec3b finalColor = c0 * (1 - rRatio) + c1 * rRatio;
+    return finalColor;
+}
 
-    clutR.convertTo(clutR, CV_32S);
-    clutG.convertTo(clutG, CV_32S);
-    clutB.convertTo(clutB, CV_32S);
+void applyHaldClut(const cv::Mat& haldImg, cv::Mat& img) {
+    int haldW = haldImg.cols, haldH = haldImg.rows;
+    int clutSize = std::cbrt(haldW * haldH);
+    float scale = (clutSize - 1) / 255.0;
 
-    // Create the filtered image
-    cv::Mat filteredImage(img.size(), CV_8UC3);
-    cv::Mat filteredImageFloat = cv::Mat::zeros(img.size(), CV_32FC3);
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            float clutR = img.at<cv::Vec3b>(i, j)[2] * scale; // BGR order
+            float clutG = img.at<cv::Vec3b>(i, j)[1] * scale;
+            float clutB = img.at<cv::Vec3b>(i, j)[0] * scale;
 
-    for (int y = 0; y < img.rows; ++y) {
-        for (int x = 0; x < img.cols; ++x) {
-            int rIndex = clutR.at<int>(y, x);
-            int gIndex = clutG.at<int>(y, x);
-            int bIndex = clutB.at<int>(y, x);
-
-            int index = rIndex + clutSize * clutSize * gIndex + clutSize * clutSize * clutSize * clutSize* bIndex;
-            cv::Vec3f color = haldImgReshaped.at<cv::Vec3f>(index);
-            filteredImageFloat.at<cv::Vec3f>(y, x) = color;
+            img.at<cv::Vec3b>(i, j) = trilinearInterpolate(haldImg, clutR, clutG, clutB, clutSize);
         }
     }
-
-    filteredImageFloat.convertTo(filteredImage, CV_8UC3);
-    return filteredImage;
 }
 
 int main() {
-    // Load images
-    cv::Mat hald = cv::imread("/home/rafael/.local/bin/luts/dehancer-fujichrome-velvia-50-k2383.png");
-    cv::Mat image = cv::imread("/home/rafael/phone/DCIM/OpenCamera/IMG_20240727_115819.jpg");
+    cv::Mat haldImg = cv::imread("../dehancer-fujichrome-velvia-50-k2383.png", cv::IMREAD_COLOR);
+    cv::Mat image = cv::imread("../orig_orig.jpg", cv::IMREAD_COLOR);
 
-    if (hald.empty() || image.empty()) {
-        std::cerr << "Could not open or find the images!" << std::endl;
-        return -1;
-    }
+    applyHaldClut(haldImg, image);
 
-    // Apply Hald CLUT
-    cv::Mat filtered = applyHaldClut(hald, image);
-
-    // Save the result
-    cv::imwrite("filtered.jpg", filtered);
-
+    cv::imwrite("filtered.jpg", image);
+    std::cout << "Application of HALD CLUT completed." << std::endl;
     return 0;
 }
-
