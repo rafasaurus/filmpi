cb22e89df9100892d63303045120c7cf727e0fbe
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-08-05 16:23:35 +0400

    Fix neon

--- cpp/main_neon.cpp
+++ cpp/main_neon.cpp
@@ -30,55 +30,37 @@ cv::Vec3b trilinearInterpolateNeon(const cv::Mat& haldImg, const float& clutR, c
     uint8x8_t c011 = vld1_u8((uint8_t*) &haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b1)));
     uint8x8_t c111 = vld1_u8((uint8_t*) &haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b1)));
 
-    // Perform the bilinear interpolation using NEON intrinsics
-    float32x4_t c00 = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c000)))), vsubq_f32(vdupq_n_f32(1.0), bRatio)),
-                                 vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c100)))), bRatio);
-    // Additional interpolation steps would follow a similar pattern...
+    // First interpolate along B axis
+    float32x4_t c00 = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c000)))), vsubq_f32(vdupq_n_f32(1.0), rRatio)),
+            vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c001)))), rRatio);
+    float32x4_t c10 = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c010)))), vsubq_f32(vdupq_n_f32(1.0), rRatio)),
+            vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c011)))), rRatio);
+    float32x4_t c01 = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c100)))), vsubq_f32(vdupq_n_f32(1.0), rRatio)),
+            vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c101)))), rRatio);
+    float32x4_t c11 = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c110)))), vsubq_f32(vdupq_n_f32(1.0), rRatio)),
+            vcvtq_f32_u32(vmovl_u16(vget_low_u16(vmovl_u8(c111)))), rRatio);
 
-    // Convert back to uchar and return
-    uint16x4_t finalColor16 = vmovn_u32(vcvtq_u32_f32(c00));
-    uint8x8_t finalColor8 = vqmovn_u16(vcombine_u16(finalColor16, finalColor16));
-    cv::Vec3b finalColor;
-    vst1_lane_u8(&finalColor[0], finalColor8, 0);
-    vst1_lane_u8(&finalColor[1], finalColor8, 1);
-    vst1_lane_u8(&finalColor[2], finalColor8, 2);
-    return finalColor;
+    // Now interpolate along G axis
+    float32x4_t c0 = vmlaq_f32(vmulq_f32(c00, vsubq_f32(vdupq_n_f32(1.0), gRatio)),
+            c10, gRatio);
+    float32x4_t c1 = vmlaq_f32(vmulq_f32(c01, vsubq_f32(vdupq_n_f32(1.0), gRatio)),
+            c11, gRatio);
+
+    // Finally interpolate along R axis
+    float32x4_t finalColor = vmlaq_f32(vmulq_f32(c0, vsubq_f32(vdupq_n_f32(1.0), bRatio)),
+            c1, bRatio);
+    // Convert from float32x4_t to uint8x8_t and pack into cv::Vec3b
+    uint16x4_t finalColor_u16 = vmovn_u32(vcvtq_u32_f32(finalColor));  // Convert 32-bit floats to 16-bit integers
+    uint8x8_t finalColor_u8 = vqmovn_u16(vcombine_u16(finalColor_u16, finalColor_u16)); // Convert and combine to 8-bit
+
+    cv::Vec3b finalVec3bColor; // Create an empty Vec3b to store the final color
+    finalVec3bColor[0] = vget_lane_u8(finalColor_u8, 0); // Blue
+    finalVec3bColor[1] = vget_lane_u8(finalColor_u8, 1); // Green
+    finalVec3bColor[2] = vget_lane_u8(finalColor_u8, 2); // Red
+    return finalVec3bColor;
 }
 #endif
 
-// Function to perform trilinear interpolation on the CLUT
-cv::Vec3b trilinearInterpolate(const cv::Mat& haldImg, float clutR, float clutG, float clutB, int clutSize) {
-    int r0 = std::floor(clutR);
-    int r1 = std::min(r0 + 1, clutSize - 1);
-    int g0 = std::floor(clutG);
-    int g1 = std::min(g0 + 1, clutSize - 1);
-    int b0 = std::floor(clutB);
-    int b1 = std::min(b0 + 1, clutSize - 1);
-
-    float rRatio = clutR - r0;
-    float gRatio = clutG - g0;
-    float bRatio = clutB - b0;
-
-    cv::Vec3b c000 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b0));
-    cv::Vec3b c100 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b0));
-    cv::Vec3b c010 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b0));
-    cv::Vec3b c110 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b0));
-    cv::Vec3b c001 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b1));
-    cv::Vec3b c101 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b1));
-    cv::Vec3b c011 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b1));
-    cv::Vec3b c111 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b1));
-
-    cv::Vec3b c00 = c000 * (1 - bRatio) + c100 * bRatio;
-    cv::Vec3b c01 = c001 * (1 - bRatio) + c101 * bRatio;
-    cv::Vec3b c10 = c010 * (1 - bRatio) + c110 * bRatio;
-    cv::Vec3b c11 = c011 * (1 - bRatio) + c111 * bRatio;
-
-    cv::Vec3b c0 = c00 * (1 - gRatio) + c10 * gRatio;
-    cv::Vec3b c1 = c01 * (1 - gRatio) + c11 * gRatio;
-
-    cv::Vec3b finalColor = c0 * (1 - rRatio) + c1 * rRatio;
-    return finalColor;
-}
 
 // Function to apply the HALD CLUT using multithreading
 void applyHaldClutPartial(const cv::Mat& haldImg, const cv::Mat& srcImg, cv::Mat& dstImg, int startRow, int endRow, int clutSize) {
@@ -91,8 +73,6 @@ void applyHaldClutPartial(const cv::Mat& haldImg, const cv::Mat& srcImg, cv::Mat
 
 #ifdef USE_NEON
             dstImg.at<cv::Vec3b>(i, j) = trilinearInterpolateNeon(haldImg, clutR, clutG, clutB, clutSize);
-#else
-            dstImg.at<cv::Vec3b>(i, j) = trilinearInterpolate(haldImg, clutR, clutG, clutB, clutSize);
 #endif
         }
     }
