25842e2b5af62e9697051420211b1ff8f18a02fe
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-01-23 18:11:47 +0400

    Faster implementation with stb library

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,53 +1,140 @@
-#include <opencv2/opencv.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <cmath>
 #include <iostream>
-#include <vector>
+#include <cstdlib>
+#include <cstring>
 
-cv::Vec3b trilinearInterpolate(const cv::Mat& haldImg, float clutR, float clutG, float clutB, int clutSize) {
-    int r0 = std::floor(clutR);
-    int r1 = std::min(r0 + 1, clutSize - 1);
-    int g0 = std::floor(clutG);
-    int g1 = std::min(g0 + 1, clutSize - 1);
-    int b0 = std::floor(clutB);
-    int b1 = std::min(b0 + 1, clutSize - 1);
+inline void trilinearInterpolate(const unsigned char* __restrict haldData, float clutR, float clutG, float clutB, int clutSize, int clutSizeSquared, unsigned char* __restrict result) {
+    int r0 = static_cast<int>(clutR);
+    int g0 = static_cast<int>(clutG);
+    int b0 = static_cast<int>(clutB);
+
+    int maxIdx = clutSize - 1;
+    int r1 = r0 + (r0 < maxIdx);
+    int g1 = g0 + (g0 < maxIdx);
+    int b1 = b0 + (b0 < maxIdx);
 
     float rRatio = clutR - r0;
     float gRatio = clutG - g0;
     float bRatio = clutB - b0;
 
-    cv::Vec3b c000 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b0));
-    cv::Vec3b c100 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b0));
-    cv::Vec3b c010 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b0));
-    cv::Vec3b c110 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b0));
-    cv::Vec3b c001 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g0 + clutSize * b1));
-    cv::Vec3b c101 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g0 + clutSize * b1));
-    cv::Vec3b c011 = haldImg.at<cv::Vec3b>(r0 + clutSize * (g1 + clutSize * b1));
-    cv::Vec3b c111 = haldImg.at<cv::Vec3b>(r1 + clutSize * (g1 + clutSize * b1));
+    int baseG0B0 = clutSize * g0 + clutSizeSquared * b0;
+    int baseG1B0 = clutSize * g1 + clutSizeSquared * b0;
+    int baseG0B1 = clutSize * g0 + clutSizeSquared * b1;
+    int baseG1B1 = clutSize * g1 + clutSizeSquared * b1;
 
-    cv::Vec3b c00 = c000 * (1 - bRatio) + c100 * bRatio;
-    cv::Vec3b c01 = c001 * (1 - bRatio) + c101 * bRatio;
-    cv::Vec3b c10 = c010 * (1 - bRatio) + c110 * bRatio;
-    cv::Vec3b c11 = c011 * (1 - bRatio) + c111 * bRatio;
+    int idx000 = (r0 + baseG0B0) * 3;
+    int idx100 = (r1 + baseG0B0) * 3;
+    int idx010 = (r0 + baseG1B0) * 3;
+    int idx110 = (r1 + baseG1B0) * 3;
+    int idx001 = (r0 + baseG0B1) * 3;
+    int idx101 = (r1 + baseG0B1) * 3;
+    int idx011 = (r0 + baseG1B1) * 3;
+    int idx111 = (r1 + baseG1B1) * 3;
 
-    cv::Vec3b c0 = c00 * (1 - gRatio) + c10 * gRatio;
-    cv::Vec3b c1 = c01 * (1 - gRatio) + c11 * gRatio;
+    float c000_r = haldData[idx000];
+    float c000_g = haldData[idx000 + 1];
+    float c000_b = haldData[idx000 + 2];
+    float c100_r = haldData[idx100];
+    float c100_g = haldData[idx100 + 1];
+    float c100_b = haldData[idx100 + 2];
+    float c010_r = haldData[idx010];
+    float c010_g = haldData[idx010 + 1];
+    float c010_b = haldData[idx010 + 2];
+    float c110_r = haldData[idx110];
+    float c110_g = haldData[idx110 + 1];
+    float c110_b = haldData[idx110 + 2];
+    float c001_r = haldData[idx001];
+    float c001_g = haldData[idx001 + 1];
+    float c001_b = haldData[idx001 + 2];
+    float c101_r = haldData[idx101];
+    float c101_g = haldData[idx101 + 1];
+    float c101_b = haldData[idx101 + 2];
+    float c011_r = haldData[idx011];
+    float c011_g = haldData[idx011 + 1];
+    float c011_b = haldData[idx011 + 2];
+    float c111_r = haldData[idx111];
+    float c111_g = haldData[idx111 + 1];
+    float c111_b = haldData[idx111 + 2];
 
-    cv::Vec3b finalColor = c0 * (1 - rRatio) + c1 * rRatio;
-    return finalColor;
+    float oneMinusB = 1.0f - bRatio;
+    float oneMinusG = 1.0f - gRatio;
+    float oneMinusR = 1.0f - rRatio;
+
+    float c00_r = c000_r * oneMinusB + c100_r * bRatio;
+    float c00_g = c000_g * oneMinusB + c100_g * bRatio;
+    float c00_b = c000_b * oneMinusB + c100_b * bRatio;
+    float c01_r = c001_r * oneMinusB + c101_r * bRatio;
+    float c01_g = c001_g * oneMinusB + c101_g * bRatio;
+    float c01_b = c001_b * oneMinusB + c101_b * bRatio;
+    float c10_r = c010_r * oneMinusB + c110_r * bRatio;
+    float c10_g = c010_g * oneMinusB + c110_g * bRatio;
+    float c10_b = c010_b * oneMinusB + c110_b * bRatio;
+    float c11_r = c011_r * oneMinusB + c111_r * bRatio;
+    float c11_g = c011_g * oneMinusB + c111_g * bRatio;
+    float c11_b = c011_b * oneMinusB + c111_b * bRatio;
+
+    float c0_r = c00_r * oneMinusG + c10_r * gRatio;
+    float c0_g = c00_g * oneMinusG + c10_g * gRatio;
+    float c0_b = c00_b * oneMinusG + c10_b * gRatio;
+    float c1_r = c01_r * oneMinusG + c11_r * gRatio;
+    float c1_g = c01_g * oneMinusG + c11_g * gRatio;
+    float c1_b = c01_b * oneMinusG + c11_b * gRatio;
+
+    result[0] = static_cast<unsigned char>(c0_r * oneMinusR + c1_r * rRatio);
+    result[1] = static_cast<unsigned char>(c0_g * oneMinusR + c1_g * rRatio);
+    result[2] = static_cast<unsigned char>(c0_b * oneMinusR + c1_b * rRatio);
 }
 
-void applyHaldClut(const cv::Mat& haldImg, cv::Mat& img) {
-    int haldW = haldImg.cols, haldH = haldImg.rows;
-    int clutSize = std::cbrt(haldW * haldH);
-    float scale = (clutSize - 1) / 255.0;
+void applyHaldClut(const unsigned char* __restrict haldData, int haldW, int haldH, unsigned char* __restrict imgData, int imgW, int imgH) {
+    int clutSize = static_cast<int>(std::cbrt(haldW * haldH));
+    int clutSizeSquared = clutSize * clutSize;
+    float scale = (clutSize - 1) / 255.0f;
 
-    for (int i = 0; i < img.rows; ++i) {
-        for (int j = 0; j < img.cols; ++j) {
-            float clutR = img.at<cv::Vec3b>(i, j)[2] * scale; // BGR order
-            float clutG = img.at<cv::Vec3b>(i, j)[1] * scale;
-            float clutB = img.at<cv::Vec3b>(i, j)[0] * scale;
+    constexpr int PREFETCH_DISTANCE = 16;
 
-            img.at<cv::Vec3b>(i, j) = trilinearInterpolate(haldImg, clutR, clutG, clutB, clutSize);
+    for (int i = 0; i < imgH; ++i) {
+        unsigned char* __restrict rowPtr = imgData + i * imgW * 3;
+        for (int j = 0; j < imgW; ++j) {
+            int pixelIdx = j * 3;
+
+            if (j + PREFETCH_DISTANCE < imgW) {
+                int prefetchPixelIdx = (j + PREFETCH_DISTANCE) * 3;
+                float prefetchClutR = rowPtr[prefetchPixelIdx] * scale;
+                float prefetchClutG = rowPtr[prefetchPixelIdx + 1] * scale;
+                float prefetchClutB = rowPtr[prefetchPixelIdx + 2] * scale;
+
+                int prefetchR0 = static_cast<int>(prefetchClutR);
+                int prefetchG0 = static_cast<int>(prefetchClutG);
+                int prefetchB0 = static_cast<int>(prefetchClutB);
+                int prefetchMaxIdx = clutSize - 1;
+                int prefetchR1 = prefetchR0 + (prefetchR0 < prefetchMaxIdx);
+                int prefetchG1 = prefetchG0 + (prefetchG0 < prefetchMaxIdx);
+                int prefetchB1 = prefetchB0 + (prefetchB0 < prefetchMaxIdx);
+
+                int prefetchBaseG0B0 = clutSize * prefetchG0 + clutSizeSquared * prefetchB0;
+                int prefetchBaseG1B0 = clutSize * prefetchG1 + clutSizeSquared * prefetchB0;
+                int prefetchBaseG0B1 = clutSize * prefetchG0 + clutSizeSquared * prefetchB1;
+                int prefetchBaseG1B1 = clutSize * prefetchG1 + clutSizeSquared * prefetchB1;
+
+                __builtin_prefetch(&haldData[(prefetchR0 + prefetchBaseG0B0) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR1 + prefetchBaseG0B0) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR0 + prefetchBaseG1B0) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR1 + prefetchBaseG1B0) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR0 + prefetchBaseG0B1) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR1 + prefetchBaseG0B1) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR0 + prefetchBaseG1B1) * 3], 0, 1);
+                __builtin_prefetch(&haldData[(prefetchR1 + prefetchBaseG1B1) * 3], 0, 1);
+            }
+
+            float clutR = rowPtr[pixelIdx] * scale;
+            float clutG = rowPtr[pixelIdx + 1] * scale;
+            float clutB = rowPtr[pixelIdx + 2] * scale;
+
+            trilinearInterpolate(haldData, clutR, clutG, clutB, clutSize, clutSizeSquared, &rowPtr[pixelIdx]);
         }
     }
 }
@@ -58,25 +145,49 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    std::string lutImagePath = argv[1];
-    std::string imagePath = argv[2];
-    std::string outputPath = argv[3];
+    const char* lutImagePath = argv[1];
+    const char* imagePath = argv[2];
+    const char* outputPath = argv[3];
 
-    cv::Mat haldImg = cv::imread(lutImagePath, cv::IMREAD_COLOR);
-    if (haldImg.empty()) {
+    int haldW, haldH, haldChannels;
+    unsigned char* haldData = stbi_load(lutImagePath, &haldW, &haldH, &haldChannels, 3);
+    if (!haldData) {
         std::cerr << "Error: Could not open or find the HALD image at " << lutImagePath << std::endl;
+        if (stbi_failure_reason()) {
+            std::cerr << "Reason: " << stbi_failure_reason() << std::endl;
+        }
         return 1;
     }
 
-    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
-    if (image.empty()) {
+    int imgW, imgH, imgChannels;
+    unsigned char* imgData = stbi_load(imagePath, &imgW, &imgH, &imgChannels, 3);
+    if (!imgData) {
         std::cerr << "Error: Could not open or find the image at " << imagePath << std::endl;
+        if (stbi_failure_reason()) {
+            std::cerr << "Reason: " << stbi_failure_reason() << std::endl;
+        }
+        stbi_image_free(haldData);
         return 1;
     }
-    cv::Mat filtered(image.size(), image.type());
 
-    applyHaldClut(haldImg, image);
+    applyHaldClut(haldData, haldW, haldH, imgData, imgW, imgH);
 
-    cv::imwrite(outputPath, image);
+    const char* ext = strrchr(outputPath, '.');
+    int success = 0;
+    if (ext && (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0)) {
+        success = stbi_write_jpg(outputPath, imgW, imgH, 3, imgData, 95);
+    } else {
+        success = stbi_write_png(outputPath, imgW, imgH, 3, imgData, imgW * 3);
+    }
+
+    if (!success) {
+        std::cerr << "Error: Failed to write output image to " << outputPath << std::endl;
+        stbi_image_free(haldData);
+        stbi_image_free(imgData);
+        return 1;
+    }
+
+    stbi_image_free(haldData);
+    stbi_image_free(imgData);
     return 0;
 }
